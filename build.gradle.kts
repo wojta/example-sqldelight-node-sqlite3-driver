3c5e85b16ff1f681ca16b64c31a25272a8eaa638
Vojtěch Sázel <wojta@users.noreply.github.com>
2023-06-14 00:24:56 +0200

    upgrade to sqldelight-node-sqlite3-driver 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.util.*
 
 plugins {
     kotlin("js") version "1.8.10"
-    id("app.cash.sqldelight") version "2.0.0-alpha05"
+    id("app.cash.sqldelight") version "2.0.0-rc01"
 }
 
 group = "cz.sazel.sqldelight.node.sqlite3"
@@ -30,7 +30,7 @@ kotlin {
         binaries.executable()
         nodejs {
             dependencies {
-                implementation("cz.sazel.sqldelight:node-sqlite3-driver-js:0.1.5")
+                implementation("cz.sazel.sqldelight:node-sqlite3-driver-js:0.2.0")
             }
         }
     }
